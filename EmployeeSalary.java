f249b98bbd7c13b2e6a4e23f3d56c55b1cc2e0bf
DurgaAkash <dadidurgaakash@gmail.com>
2026-08-05 12:19:59 +0530

    Add files via upload

---
+++ EmployeeSalary.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+class EmployeeSalary {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the no of employees:");
+        int n=sc.nextInt();
+        int []arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println(" Updated Salaries of Employees");
+        IntStream.range(0,arr.length)
+                .map(i->arr[i]+(arr[i]*10/100))
+                .forEach(System.out::println);
+    }
+}
+
